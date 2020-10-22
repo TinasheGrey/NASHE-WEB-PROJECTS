1e52137774f461e004377afd8583177c26dc21b8
TinasheGrey <62029920+TinasheGrey@users.noreply.github.com>
2020-10-22 13:13:39 -0700

    Add files via upload
    Put all the files in one folder. they all form a complete font end website of a restaurant

---
+++ main.js
@@ -0,0 +1,7 @@
+var restaurantName= ' THANK YOU,';
+
+var remarksName =' VISIT US AGAIN';
+
+document.getElementById('maintitle').innerHTML= "CHICKENLINE RESTAURANT IS THE HOTTEST  FOOD PLACE IN MANGOCHI.  " + restaurantName + remarksName;
+
+Console.log(restaurantName + remarksName);
